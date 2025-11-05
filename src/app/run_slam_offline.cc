caa12bf5e5f455536a200e5d5e6df3dcfcc3ae91
Wangshihu12 <852066173@qq.com>
2025-11-05 10:07:29 +0800

    修改了默认的参数以及中文的显示

--- src/app/run_slam_offline.cc
+++ src/app/run_slam_offline.cc
@@ -5,17 +5,29 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
+#ifdef _WIN32
+#define NOMINMAX  // 防止 Windows.h 定义 min/max 宏，避免与 C++ 标准库冲突
+#include <windows.h>
+#endif
+
 #include "core/system/slam.h"
 #include "io/yaml_io.h"
 #include "ui/pangolin_window.h"
 #include "utils/timer.h"
 #include "wrapper/bag_io.h"
 
-DEFINE_string(input_bag, "", "输入数据包路径");
-DEFINE_string(config, "./config/default.yaml", "配置文件");
+DEFINE_string(input_bag, "F:/mid360/m40_2025-09-08-07-41-42.goslam", "输入数据包路径");
+DEFINE_string(config, "F:/github/lightning-lm/config/default.yaml", "配置文件");
 
 /// 运行一个LIO前端，带可视化
 int main(int argc, char** argv) {
+#ifdef _WIN32
+    // 设置控制台输出为 UTF-8 编码
+    SetConsoleOutputCP(CP_UTF8);
+    // 设置控制台输入为 UTF-8 编码（可选）
+    SetConsoleCP(CP_UTF8);
+#endif
+
     google::InitGoogleLogging(argv[0]);
     FLAGS_colorlogtostderr = true;
     FLAGS_stderrthreshold = google::GLOG_INFO;
